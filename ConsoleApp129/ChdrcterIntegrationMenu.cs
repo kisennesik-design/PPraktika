4a8a7628f2cfa0aa8892b5f57292408e0f9a0130
stud <stud@DESKTOP-JM1SP2K>
2025-02-14 12:15:04 +0700

    Reapply "Добавлены "Главное меню" и  "Меню взаимодействия""
    This reverts commit 4ca5f5f2ea2c7d7ce649635d6e3277acb865e660.

---
+++ ConsoleApp129/ChdrcterIntegrationMenu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp129
+{
+    public class CharacterInteractionMenu
+    {
+        private ConsoleColor textColor = ConsoleColor.DarkRed;
+        public void ShowInteractionMenu(Hero hero, Person person)
+        {
+            string[] menuItems = { "Поговорить (не реализовано)", "Атаковать (не реализовано)", "Назад" };
+            int selectedIndex = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Взаимодействие с {person.Rendering_on_the_map()}");
+
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    if (i == selectedIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine($"> {menuItems[i]}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = textColor;
+                        Console.WriteLine($"  {menuItems[i]}");
+                        Console.ResetColor();
+                    }
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedIndex = (selectedIndex - 1 + menuItems.Length) % menuItems.Length;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedIndex = (selectedIndex + 1) % menuItems.Length;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (selectedIndex == 0)
+                        {
+                            Console.ForegroundColor = textColor;
+                            Console.WriteLine("Попытка поговорить... (не реализовано)");
+                            Console.ResetColor();
+                            Console.ReadKey(true);
+                        }
+                        else if (selectedIndex == 1)
+                        {
+                            Console.ForegroundColor = textColor;
+                            Console.WriteLine("Попытка атаковать... (не реализовано)");
+                            Console.ResetColor();
+                            Console.ReadKey(true);
+                        }
+                        else if (selectedIndex == 2)
+                        {
+                            return;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp129/MainMenu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp129
+{
+    public class MainMenu
+    {
+        public void ShowMenu(Map gameMap)
+        {
+            string[] menuItems = { "Начать игру", "Выход" };
+            int selectedIndex = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Главное меню ===");
+
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    if (i == selectedIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.WriteLine($"> {menuItems[i]}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {menuItems[i]}");
+                    }
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedIndex = (selectedIndex - 1 + menuItems.Length) % menuItems.Length;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedIndex = (selectedIndex + 1) % menuItems.Length;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (selectedIndex == 0)
+                        {
+                            StartGame(gameMap);
+                            return;
+                        }
+                        else if (selectedIndex == 1)
+                        {
+                            Console.WriteLine("Выход из игры...");
+                            Environment.Exit(0);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void StartGame(Map gameMap)
+        {
+            gameMap.GenerateMap();
+            while (true)
+            {
+                Console.Clear();
+                gameMap.DrawMap();
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                gameMap.MovePersons(keyInfo.Key);
+                gameMap.MovePersons();
+
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+    }
+}

070dcc8af63fa9f788b4cb9ac8b69f7b87fe3b19
stud <stud@DESKTOP-JM1SP2K>
2025-02-14 10:59:35 +0700

    Add Main

--- ConsoleApp129/Person.cs
+++ ConsoleApp129/Person.cs
@@ -6,43 +6,60 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp129
 {
+    // Класс, представляющий базового персонажа на карте, наследует MapObject. 
     internal class Person : MapObject
     {
+        // Координаты персонажа на карте. 
         int pointX;
         int pointY;
 
-        public Person(int X,int Y)
+        // Конструктор, инициализирующий координаты персонажа. 
+        // Параметры X и Y определяют позицию на карте. 
+        public Person(int X, int Y)
         {
-            pointX = X; pointY = Y; 
+            pointX = X; pointY = Y;
         }
+        // Переопределяет абстрактный метод для отображения персонажа на карте. 
+        // В базовом классе персонаж отображается как символ '!'. 
         public override char Rendering_on_the_map()
         {
-            return '☺';
+            return '!'; // Символ, представляющий персонажа на карте. 
         }
     }
 
+    // Класс, представляющий героя на карте, наследует Person. 
     internal class Hero : Person
     {
+        // Конструктор, который передает координаты в конструктор базового класса. 
+        // Герой будет размещен на карте на позиции X, Y. 
         public Hero(int X, int Y) : base(X, Y)
         {
 
         }
+        // Переопределяет абстрактный метод для отображения героя на карте. 
+        // Герой отображается как символ '@' с желтым цветом. 
+
         public override char Rendering_on_the_map()
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            return '☻';
+            Console.ForegroundColor = ConsoleColor.Yellow; // Устанавливаем желтый цвет для героя. 
+            return '@'; ;  // Символ для отображения героя. 
         }
     }
-
+    // Класс, представляющий врага на карте, наследует Person. 
     internal class Enemy : Person
     {
+        // Конструктор, который передает координаты в конструктор базового класса. 
+        // Враг будет размещен на карте на позиции X, Y. 
         public Enemy(int X, int Y) : base(X, Y)
         {
         }
+        // Переопределяет абстрактный метод для отображения врага на карте. 
+        // Враг отображается как символ '!' с красным цветом. 
         public override char Rendering_on_the_map()
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            return base.Rendering_on_the_map();
+            Console.ForegroundColor = ConsoleColor.Red;// Устанавливаем красный цвет для врага. 
+            return base.Rendering_on_the_map(); // Используем символ из базового класса для отображения 
+            врага.
         }
     }
 }
--- ConsoleApp129/Program.cs
+++ ConsoleApp129/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map gameMap = new Map();
+            gameMap.Map_generation();
+
+
+            while (true)
+            {
+                Console.Clear();
+                gameMap.Drawing_the_map();
+
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+
+                gameMap.MovePersons(keyInfo.Key);
+
+
+                gameMap.MovePersons();
+
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
